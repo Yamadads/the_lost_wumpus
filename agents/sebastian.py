919023ec182f9221e3910d532a33df3e405ba084
Yamadads <spawlak@yahoo.com>
2017-04-16 00:55:32 +0200

    zdane1

--- agents/sebastian.py
+++ agents/sebastian.py
@@ -1,18 +1,12 @@
-# Przykladowy agent do zadania 'zagubiony Wumpus'. Agent porusza sie wezykiem.
-
 import random
+import numpy as np
 from action import Action
+from numpy import unravel_index
+import copy
 
-# nie zmieniac nazwy klasy
 class Agent:
 
-    # nie zmieniac naglowka konstruktora, tutaj agent dostaje wszystkie informacje o srodowisku
     def __init__(self, p, pj, pn, height, width, areaMap):
-
-        self.times_moved = 0
-        self.direction = Action.LEFT
-
-        # w ten sposob mozna zapamietac zmienne obiektu
         self.p = p
         self.pj = pj
         self.pn = pn
@@ -20,24 +14,160 @@ class Agent:
         self.width = width
         self.map = areaMap
 
-        # w tym przykladzie histogram wypelniany jest tak aby na planszy wyszedl gradient
         self.hist = []
+        self.temp_hist = []
         for y in range(self.height):
             self.hist.append([])
             for x in range(self.width):
-                self.hist[y].append(float(y + x) / (self.width + self.height - 2))
+                self.hist[y].append(1.0)
+        self._normalize_hist()
+        self.max_prob_threshold = self.hist[0][0]*2
+        self.move_dir = {}
+        self.move_dir[Action.UP] = [0,1]
+        self.move_dir[Action.DOWN] = [0,-1]
+        self.move_dir[Action.LEFT] = [1,0]
+        self.move_dir[Action.RIGHT] = [-1,0]
 
-        # dopisac reszte inicjalizacji agenta
+        self.to_move_dir = {}
+        self.to_move_dir[Action.DOWN] = [0,1]
+        self.to_move_dir[Action.UP] = [0,-1]
+        self.to_move_dir[Action.LEFT] = [-1,0]
+        self.to_move_dir[Action.RIGHT] = [1,0]
+
+        self.prob_param = {}
+        self.prob_param['J',True] = self.pj
+        self.prob_param['J',False] = 1.0 - self.pj
+        self.prob_param['.',True] = self.pn
+        self.prob_param['.',False] = 1.0 - self.pn
+        self.prob_param['W',True] = self.pn
+        self.prob_param['W',False] = 1.0 - self.pn
+
+        self.exit_position = [-1,-1]
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.map[i][j] == 'W':
+                    self.exit_position = [i,j]
+                    break
+            if self.exit_position != [-1,-1]:
+                break
+
+        self.times_moved = 0
+        self.direction = Action.LEFT
+        self.general_times_moved = 0
+        self.target_moves = []
         return
 
-    # nie zmieniac naglowka metody, tutaj agent dokonuje obserwacji swiata
-    # sensor przyjmuje wartosc True gdy agent ma uczucie stania w jamie
-    def sense(self, sensor):
-        pass
+    def _update_hist_move(self, move):
+        self.temp_hist = copy.deepcopy(self.hist)
+        for row in range(len(self.hist)):
+            for i in range(len(self.hist[0])):
+                self.hist[row][i] = self._calc_prob_in_field(row,i,move)
+        self._normalize_hist()
 
-    # nie zmieniac naglowka metody, tutaj agent decyduje w ktora strone sie ruszyc,
-    # funkcja MUSI zwrocic jedna z wartosci [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT]
+    def _calc_prob_in_field(self, y, x, action):
+        prob_sum = 0
+        x += self.move_dir[action][0]
+        y += self.move_dir[action][1]
+        for i in self.move_dir:
+            prob_sum += self._get_cyclic_temp_hist_value(y+self.move_dir[i][1],x+self.move_dir[i][0])*((1.0-self.p)/4)
+        prob_sum += self._get_cyclic_temp_hist_value(y,x)*self.p
+        return prob_sum
+
+    def _get_cyclic_temp_hist_value(self, y, x):
+        return self.temp_hist[y%self.height][x%self.width]
+
+    def _update_hist_sense(self, sensor_result):
+        for row in xrange(len(self.hist)):
+            for i in range(len(self.hist[0])):
+                self.hist[row][i] *= self.prob_param[self.map[row][i],sensor_result]
+        self._normalize_hist()
+
+    def _normalize_hist(self):
+        hist_sum = float(np.sum(self.hist))
+        for row in range(len(self.hist)):
+            for i in range(len(self.hist[0])):
+                self.hist[row][i] /= hist_sum
+
+    def sense(self, sensor):
+        self._update_hist_sense(sensor)
+        return dir
+
     def move(self):
+        #self.general_times_moved += 1
+        a = np.array(self.hist)
+        max_prob_idx = unravel_index(a.argmax(), a.shape)
+        max_prob = self.hist[max_prob_idx[0]][max_prob_idx[1]]
+        if (max_prob) > self.max_prob_threshold:
+            dir = self._get_best_move(max_prob_idx)
+            self._check_move(dir,max_prob_idx)
+        else:
+            dir = self._snake_move()
+        self._update_hist_move(dir)
+        return dir
+
+    def _check_move(self, move, actual_position):
+        if len(self.target_moves)>5:
+            if (self.target_moves[-3:]==[Action.DOWN,Action.UP,Action.DOWN]):
+                self.hist[actual_position[0]][actual_position[1]]*=0.2
+                self._normalize_hist()
+            if (self.target_moves[-3:]==[Action.UP,Action.DOWN,Action.UP]):
+                self.hist[actual_position[0]][actual_position[1]]*=0.2
+                self._normalize_hist()
+            if (self.target_moves[-3:]==[Action.LEFT,Action.RIGHT,Action.LEFT]):
+                self.hist[actual_position[0]][actual_position[1]]*=0.2
+                self._normalize_hist()
+            if (self.target_moves[-3:]==[Action.RIGHT,Action.LEFT,Action.RIGHT]):
+                self.hist[actual_position[0]][actual_position[1]]*=0.2
+                self._normalize_hist()
+        self.target_moves.append(move)
+
+
+
+    def _get_trace_to_target(self, actual_position):
+        self.target_moves = []
+        while True:
+            move = self._get_best_move(actual_position)
+            if move == None:
+                break
+            self.target_moves.append(move)
+            new_position = (actual_position[0] +self.to_move_dir[move][1], actual_position[1] +self.to_move_dir[move][0])
+            actual_position = new_position
+        if (len(self.target_moves)==0):
+            self.target_moves.append(Action.DOWN)
+
+    def _get_best_move(self, actual_position):
+        actual_x = actual_position[1]
+        actual_y = actual_position[0]
+        target_x = self.exit_position[1]
+        target_y = self.exit_position[0]
+        z_x = target_x-actual_x
+        z_y = target_y-actual_y
+
+        if z_x > 0:
+            if z_x > (0.5*self.width):
+                return Action.LEFT
+            else:
+                return Action.RIGHT
+        elif z_x < 0:
+            if abs(z_x) > (0.5*self.width):
+                return Action.RIGHT
+            else:
+                return Action.LEFT
+
+        if z_y > 0:
+            if z_y > (0.5*self.height):
+                return Action.UP
+            else:
+                return Action.DOWN
+        elif z_y <0:
+            if abs(z_y) > (0.5*self.height):
+                return Action.DOWN
+            else:
+                return Action.UP
+        return random.choice([Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT])
+
+
+    def _snake_move(self):
         if self.times_moved < self.width - 1:
             self.times_moved += 1
             return self.direction
@@ -46,9 +176,6 @@ class Agent:
             self.direction = Action.RIGHT if self.direction == Action.LEFT else Action.LEFT
             return Action.DOWN
 
-    # nie zmieniac naglowka metody, tutaj agent udostepnia swoj histogram (ten z filtru
-    # histogramowego), musi to byc tablica (lista list, krotka krotek...) o wymarach takich jak
-    # plansza, pobranie wartosci agent.histogram()[y][x] zwraca prawdopodobienstwo stania na polu
-    # w wierszu y i kolumnie x
     def histogram(self):
+        #print(self.hist)
         return self.hist
